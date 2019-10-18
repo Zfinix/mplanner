2cd0094070630bd94ce5647efd7a9d90430f46d6
Chiziaruhoma <ogbondachiziaruhoma@gmail.com>
2019-10-18 12:02:29 +0100

    finished changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:mplanner/views/intersit/intersitPage.dart';
 
-
-
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'MPlanner',
